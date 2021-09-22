537d6ead7fd18208e5c8c234573f1914f2f13e8a
aliniko <aliverchan@gmail.com>
2021-09-22 22:22:10 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let url = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json'
+let req = new XMLHttpRequest()
+
+// namaing the varaibles we need them later
+let data 
+let values
